85ec725256f7d946ba3cedd420ac7cd03cf48807
LeptonAlvarado <oscarjosue_alavrador@ucol.mx>
2019-07-02 15:55:00 -0500

    Aver

--- Ultrasonido_Ejemplo1.py
+++ Ultrasonido_Ejemplo1.py
@@ -27,6 +27,11 @@ def balanceDeBlancos (imagen, maxPixel):
 # Con este codigo se muestra la imagen tal cual esta
 ultSoundOriginal = cv2.imread('prueba.jpg', 0)
 
+ret,thresh1 = cv2.threshold(ultSoundOriginal,0,1,cv2.THRESH_BINARY_INV)
+cv2.imshow('Sobel y', thresh1)
+cv2.waitKey(0)
+cv2.destroyAllWindows();
+'''
 laplacian = cv2.Laplacian(ultSoundOriginal,cv2.CV_64F)
 sobelx = cv2.Sobel(ultSoundOriginal,cv2.CV_64F,1,0,ksize=3)
 sobely = cv2.Sobel(ultSoundOriginal,cv2.CV_64F,0,1,ksize=3)
@@ -36,13 +41,13 @@ cv2.imshow('Sobel x', sobelx)
 cv2.imshow('Sobel y', sobely)
 cv2.waitKey(0)
 cv2.destroyAllWindows();
-
+'''
 # X
-sobelx64f = cv2.Sobel(ultSoundOriginal,cv2.CV_64F,1,0,ksize=3)
+sobelx64f = cv2.Sobel(ultSoundOriginal,cv2.CV_64F,1,0,ksize=5)
 abs_sobel64f = np.absolute(sobelx64f)
 sobel_8x = np.uint8(abs_sobel64f)
 # Y
-sobelx64f = cv2.Sobel(ultSoundOriginal,cv2.CV_64F,0,1,ksize=3)
+sobelx64f = cv2.Sobel(ultSoundOriginal,cv2.CV_64F,0,1,ksize=5)
 abs_sobel64f = np.absolute(sobelx64f)
 sobel_8y = np.uint8(abs_sobel64f)
 
@@ -50,12 +55,20 @@ cv2.imshow('Sobel x x64', sobel_8x)
 cv2.imshow('Sobel y x64', sobel_8y)
 cv2.waitKey(0)
 cv2.destroyAllWindows();
-
+'''
+_,th = cv2.threshold(sobel_8y,127,255,0)
+imagen, contornos, jerarquia = cv2.findContours(th, cv2.RETR_TREE, cv2.CHAIN_APPROX_SIMPLE)
+cv2.drawContours(sobel_8y, contornos, -1, (0,255,0), 3)
+cv2.imshow('Sobel y x64', sobel_8y)
+cv2.waitKey(0)
+cv2.destroyAllWindows();
+'''
 #valorMaxImagen (ultSoundOriginal)
-
+'''
 # Cany
 
 bordes = cv2.Canny(ultSoundOriginal,0,100)
 cv2.imshow('Caby', bordes)
 cv2.waitKey(0)
 cv2.destroyAllWindows();
+'''
