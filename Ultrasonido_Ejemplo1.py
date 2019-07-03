b1914577ee4e5d7871316d165952f9d4e33fff41
LeptonAlvarado <oscarjosue_alavrador@ucol.mx>
2019-07-03 12:02:27 -0500

    Se obtienen las coordenadas de los contornos

--- Ultrasonido_Ejemplo1.py
+++ Ultrasonido_Ejemplo1.py
@@ -26,11 +26,19 @@ cv2.destroyAllWindows()
 # Se hara un cierre de los blancos para posteriormente detectar bordes
 kernel = np.ones((3,3),np.uint8)
 cierre = cv2.morphologyEx(umbralUlt, cv2.MORPH_CLOSE, kernel)
-cv2.imshow('Umbral', cierre )
+cv2.imshow('Cierre', cierre )
 cv2.waitKey(0)
 cv2.destroyAllWindows()
 
+# Deteccion de borde
+contours, hierarchy = cv2.findContours(cierre, cv2.RETR_EXTERNAL, cv2.CHAIN_APPROX_SIMPLE)
+cv2.drawContours(ultSoundOriginal, contours, -1, (255, 0, 0), 2)
+cv2.imshow('Contornos', cierre )
+cv2.imshow('dx', ultSoundOriginal )
+cv2.waitKey(0)
+cv2.destroyAllWindows()
 
+print(contours)
 
 '''
 laplacian = cv2.Laplacian(ultSoundOriginal,cv2.CV_64F)
@@ -42,7 +50,7 @@ cv2.imshow('Sobel x', sobelx)
 cv2.imshow('Sobel y', sobely)
 cv2.waitKey(0)
 cv2.destroyAllWindows();
-'''
+
 # X
 sobelx64f = cv2.Sobel(ultSoundOriginal,cv2.CV_64F,1,0,ksize=5)
 abs_sobel64f = np.absolute(sobelx64f)
@@ -56,16 +64,16 @@ cv2.imshow('Sobel x x64', sobel_8x)
 cv2.imshow('Sobel y x64', sobel_8y)
 cv2.waitKey(0)
 cv2.destroyAllWindows();
-'''
+
 _,th = cv2.threshold(sobel_8y,127,255,0)
 imagen, contornos, jerarquia = cv2.findContours(th, cv2.RETR_TREE, cv2.CHAIN_APPROX_SIMPLE)
 cv2.drawContours(sobel_8y, contornos, -1, (0,255,0), 3)
 cv2.imshow('Sobel y x64', sobel_8y)
 cv2.waitKey(0)
 cv2.destroyAllWindows();
-'''
+
 #valorMaxImagen (ultSoundOriginal)
-'''
+
 # Cany
 
 bordes = cv2.Canny(ultSoundOriginal,0,100)
