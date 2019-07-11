67e0abffa8cc76c67380722c506e4bf3dce9035d
LeptonAlvarado <oscarjosue_alavrador@ucol.mx>
2019-07-11 17:43:22 -0500

    Se creo una funcion para los nombres de la imagen

--- Sistema_Automatizado.py
+++ Sistema_Automatizado.py
@@ -3,24 +3,40 @@ import cv2            # Libreria para hacer el procesamiento de imagenes
 import numpy as np    # Libreria para trabajar con las matrices de las imagenes
 import os
 
-imgList = []
-# Esta variable guarda la direccion del archivo
-# Se tomo hasta el nombre de la imagen menos los ultimos numeros
+def nombreImagen (path_carpeta):
+    imgList = []
+    items = os.listdir(path_carpeta)
+    for file in items:
+        # Si todo esta bien entrara a esta parte del codigo
+         try:
+            # En esta parte se lee la imagen
+            nombre = file
+            # Dentro del parentesis se pone todo lo que esta antes de los numeros del nombre de la imagen
+            separacion = nombre.split('IT SandraITXXXX0E_Frame')
+            # Se pone int al comienzo para convertirlo a una variable de punto entero
+            # En la variable separacionse le pone [1] ya que el vaalor [0] es todo lo que esta antes del numero
+            # Ahora en la variable numero se guarda el split de separacion pero esta ves el [0] que contiene el numero
+            # Ya que el [1] contendria la direccion del archivo
+            numero = int(separacion[1].split('.jpg')[0])
+            imgList.append(numero)
+        # En caso de que encuentre un problema entrera a esta parte 
+        except:
+            print('Cant import ' + file)
+    
+    # Regresa solo los numeros del nombre de la imagen en una lista de enteros
+    return (imgList)
+
+# Esta variable guarda la direccion de los archivos
 my_path = 'C:/Users/josue/OneDrive/Escritorio/Ultrasonido/Dummi Right/Series_1/'
-items = os.listdir('C:/Users/josue/OneDrive/Escritorio/Ultrasonido/Dummi Right/Series_1/')
 # Esta variable almacena la funcion glob la cual buscara todos los archivos con terminacion .jpg
 files = glob.glob(my_path + '*.jpg')
 
 # Aqui se leeran todos los archivos para poder trabajar con ellos y finalmente guardarlos
 # Esta funcion lee las imagenes de forma 1,10,100,101,102,...,109,11,110,111,...
-for file in items:
+for file in files:
     # Si todo esta bien entrara a esta parte del codigo
     try:
         # En esta parte se lee la imagen
-        nombre = file
-        separacion = nombre.split('IT SandraITXXXX0E_Frame')
-        numero = int(separacion[1].split('.jpg')[0])
-        imgList.append(numero)
         ultraSonido = cv2.imread(file,0)
         # Se recorta la imagen, para que solo quede la parte donde se encuentra el ultrasonido
         ultSonTrimm = ultraSonido[76:517, 236:685] 
@@ -34,6 +50,5 @@ for file in items:
     except:
         print('Cant import ' + file)
 
-imgList.sort()
-print(imgList)
+# imgList.sort()
 
