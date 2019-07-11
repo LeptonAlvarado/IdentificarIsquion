b992056b8bf600234240c526a70ac0304ca5fae5
LeptonAlvarado <oscarjosue_alavrador@ucol.mx>
2019-07-11 14:00:01 -0500

    Se intento usar la libreria os

--- Sistema_Automatizado.py
+++ Sistema_Automatizado.py
@@ -1,21 +1,21 @@
 import glob           # Libreria para leer los archivos
 import cv2            # Libreria para hacer el procesamiento de imagenes
 import numpy as np    # Libreria para trabajar con las matrices de las imagenes
+import os
+
 
 # Esta variable guarda la direccion del archivo
 # Se tomo hasta el nombre de la imagen menos los ultimos numeros
-my_path = 'C:/Users/josue/OneDrive/Escritorio/Ultrasonido/Dummi Right/Series_1/IT SandraITXXXX0E_Frame'
-
+my_path = 'C:/Users/josue/OneDrive/Escritorio/Ultrasonido/Dummi Right/Series_1/'
+items = os.listdir('C:/Users/josue/OneDrive/Escritorio/Ultrasonido/Dummi Right/Series_1/')
 # Esta variable almacena la funcion glob la cual buscara todos los archivos con terminacion .jpg
 files = glob.glob(my_path + '*.jpg')
 
 # Aqui se leeran todos los archivos para poder trabajar con ellos y finalmente guardarlos
 # Esta funcion lee las imagenes de forma 1,10,100,101,102,...,109,11,110,111,...
-for file in files:
+for file in items:
     # Si todo esta bien entrara a esta parte del codigo
     try:
-        i=0
-        i+=1
         # En esta parte se lee la imagen
         ultraSonido = cv2.imread(file,0)
         # Se recorta la imagen, para que solo quede la parte donde se encuentra el ultrasonido
@@ -23,9 +23,8 @@ for file in files:
         # De esa parte se recorta para dejar solo la parte del hueso
         isquionTrimm = ultSonTrimm[0:168, 0:449]
         cv2.imshow('aver',isquionTrimm)
-        cv2.waitKey(0)
+        cv2.waitKey(100)
         cv2.destroyAllWindows()
-        #print(files & '/n')
         # Al final se debe crear otra carpeta con los nombres del archivo
     # En caso de que encuentre un problema entrera a esta parte    
     except:
