bdfd298865493c95a9f4f46ab283bb055c11c73d
LeptonAlvarado <oscarjosue_alavrador@ucol.mx>
2019-07-11 17:16:07 -0500

    Se logra obtener solo los numeros del nombre de la imagen

--- Sistema_Automatizado.py
+++ Sistema_Automatizado.py
@@ -3,7 +3,7 @@ import cv2            # Libreria para hacer el procesamiento de imagenes
 import numpy as np    # Libreria para trabajar con las matrices de las imagenes
 import os
 
-
+imgList = []
 # Esta variable guarda la direccion del archivo
 # Se tomo hasta el nombre de la imagen menos los ultimos numeros
 my_path = 'C:/Users/josue/OneDrive/Escritorio/Ultrasonido/Dummi Right/Series_1/'
@@ -17,15 +17,23 @@ for file in items:
     # Si todo esta bien entrara a esta parte del codigo
     try:
         # En esta parte se lee la imagen
+        nombre = file
+        separacion = nombre.split('IT SandraITXXXX0E_Frame')
+        numero = int(separacion[1].split('.jpg')[0])
+        imgList.append(numero)
         ultraSonido = cv2.imread(file,0)
         # Se recorta la imagen, para que solo quede la parte donde se encuentra el ultrasonido
         ultSonTrimm = ultraSonido[76:517, 236:685] 
         # De esa parte se recorta para dejar solo la parte del hueso
         isquionTrimm = ultSonTrimm[0:168, 0:449]
         cv2.imshow('aver',isquionTrimm)
-        cv2.waitKey(100)
+        cv2.waitKey(0)
         cv2.destroyAllWindows()
         # Al final se debe crear otra carpeta con los nombres del archivo
-    # En caso de que encuentre un problema entrera a esta parte    
+    # En caso de que encuentre un problema entrera a esta parte 
     except:
         print('Cant import ' + file)
+
+imgList.sort()
+print(imgList)
+
